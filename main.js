0a7fb23465ced98b7c5b8283470d877455e47f5f
nadstrr <nadia.alton89@gmail.com>
2025-12-26 15:47:56 -0600

    added 67 box

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-
+function checkNumber() {
+    const input = document.getElementById('numberInput');
+    const result = document.getElementById('result');
+    const number = parseInt(input.value);
+    
+    if (number === 67) {
+        result.textContent = "slay qween";
+    } else {
+        result.textContent = "wrong! choose again";
+    }
+}
